17e35ca834a1fc8159016c20d8f1ecce58f14a00
4yume <ykozuma@byupathway.edu>
2026-05-07 20:44:02 +0900

    learning activity

--- code-along-header/scripts/navigation.js
+++ code-along-header/scripts/navigation.js
@@ -4,4 +4,11 @@ const navbutton = document.querySelector('#ham-btn');
 //toggle the show class off and on
 navbutton.addEventListener('click', () => {
     navbutton.classList.toggle('show');
-})
+});
+
+const navBar = document.querySelector('#nav-bar');
+
+navbutton.addEventListener('click', () => {
+    navbutton.classList.toggle("show");
+    navBar.classList.toggle('show');
+});
