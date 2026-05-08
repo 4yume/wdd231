67e1b6831e47d1a063d93e93f5f4b177d9db502a
4yume <ykozuma@byupathway.edu>
2026-05-08 17:30:59 +0900

    add css for small screen

---
+++ scripts/base.js
@@ -0,0 +1,7 @@
+const navbutton = document.querySelector('#ham-btn');
+const navbar = document.querySelector('#nav-bar');
+
+navbutton.addEventListener('click', () => {
+    navbutton.classList.toggle('show');
+    navbar.classList.toggle('show');
+});
