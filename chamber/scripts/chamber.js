43e67730d4349dd7377b76f5c96c1608d677b6ae
4yume <ykozuma@byupathway.edu>
2026-05-16 12:34:10 +0900

    change the button issues

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -2,8 +2,14 @@ const navbutton = document.querySelector('#ham-btn');
 const navbar = document.querySelector('#nav-bar');
 
 navbutton.addEventListener('click', () => {
-    navbutton.classList.toggle('show');
     navbar.classList.toggle('show');
+
+    if (navbar.classList.contains("show")) {
+        navbutton.textContent = "X";
+    }
+    else {
+        navbutton.textContent = "≣";
+    }
 });
 
 
