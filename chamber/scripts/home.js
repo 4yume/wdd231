99c6b5520370ddadf609cbd22060c7069bda0a54
4yume <ykozuma@byupathway.edu>
2026-05-22 15:23:01 +0900

    adding weather and spotlits section

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -5,13 +5,16 @@ const currentTemp = document.querySelector('#temp');
 const desc = document.querySelector('#desc');
 const humidity = document.querySelector('#humidity');
 
+const forecastContainer = document.querySelector('#forecast-container');
+
 
 const appid = "f55791f557bf97fb5986aaf09e76ec91"
-const wurl = `https://api.openweathermap.org/data/2.5/weather?lat=49.74&lon=6.64&appid=${appid}&units=imperial`
+const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=49.74&lon=6.64&appid=${appid}&units=imperial`
+const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=49.74&lon=6.64&appid=${appid}&units=imperial`
 
 async function apiFetch() {
     try {
-        const response = await fetch(wurl);
+        const response = await fetch(weatherUrl);
         if (response.ok) {
             const data = await response.json();
             console.log(data);
@@ -35,3 +38,96 @@ function displayResults(data) {
     weatherIcon.setAttribute('alt', desc);
     humidity.innerHTML = `Humidity: ${data.main.humidity}`;
 }
+
+async function getForcast() {
+    try {
+        const response = await fetch(forecastUrl);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayForecast(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function displayForecast(data) {
+    const forecast = data.list.filter(item =>
+        item.dt_txt.includes('12:00:00')
+    );
+
+    forecast.slice(0, 3).forEach(day => {
+        const forecastDate = new Date(day.dt_txt);
+        const dayName = forecastDate.toLocaleDateString('en-US', {
+            weekday: 'long'
+        });
+
+        const temperature = Math.round(day.main.temp);
+        const forecastCard = document.createElement('div');
+
+        forecastCard.innerHTML = `
+        <p>${dayName}: <span>${temperature}&deg;F</span></p>
+        `;
+
+        forecastContainer.appendChild(forecastCard);
+    });
+}
+
+getForcast();
+
+
+//spotlight
+const spotlight = document.querySelector('#spotlight-container');
+const membersUrl = 'data/members.json';
+
+async function getSpotlights() {
+    try {
+        const response = await fetch(membersUrl);
+        if (response.ok) {
+            const data = await response.json();
+            displaySpotlights(data);
+        }
+        else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function displaySpotlights(data){
+    const filterMembers = data.filter(member =>
+        member.level === 2 || member.level === 3
+    );
+
+    const selectedMembers = [];
+    while (selectedMembers.length < 3) {
+        const randomIndex = Math.floor(Math.random() * filterMembers.length);
+
+        const randomMember = filterMembers[randomIndex];
+
+        if (!selectedMembers.includes(randomMember)) {
+            selectedMembers.push(randomMember);
+        }
+    }
+     
+    selectedMembers.forEach(member => {
+        const card = document.createElement('section');
+        card.innerHTML = `
+        <p>${member.name}</p>
+        <img src="images/${member.image}" alt="${member.name} logo">
+        <p>${member.address}</p>
+        <p>${member.phone}</p>
+        <p>${member.url}</p>
+        <p>${member.level === 3 ? 'Gold Member' : 'Silver Member'}</p>
+        `;
+
+        spotlight.appendChild(card);
+    });
+}
+
+getSpotlights();
