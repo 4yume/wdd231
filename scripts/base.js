14f0f86d595ed23f556a75f596a788caa2962837
4yume <ykozuma@byupathway.edu>
2026-05-26 16:17:14 +0900

    add dialog

--- scripts/base.js
+++ scripts/base.js
@@ -149,3 +149,30 @@ document.querySelector('#wdd').addEventListener("click", () => {
 
     displayCourses(wddCourses);
 });
+
+
+
+
+//dialog
+function displayCourseDetails(course) {
+    courseDetails.innerHTML = "";
+    courseDetails.innerHTML = `
+    <button id="closeModal">❌</button>
+    <h2>${course.subject} ${course.number}</h2>
+    <h3>${course.title}</h3>
+    <p><strong>Credits</strong>: ${course.credits}</p>
+    <p><strong>Certificate</strong>: ${course.certificate}</p>
+    <p>${course.description}</p>
+    <p><strong>Technologies</strong>: ${course.technology.join(', ')}</p>
+    `;
+
+    courseDetails.showModal();
+
+    closeModal.addEventListener("click", () => {
+        courseDetails.close();
+    });
+}
+
+courseDiv.addEventListener('click', () => {
+    displayCourseDetails(course);
+});
