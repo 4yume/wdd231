0fedf4be87247b44578f4e62ce013c0a7bcb56f8
4yume <ykozuma@byupathway.edu>
2026-06-13 11:34:52 +0900

    adding footer in attribution and thanks file

--- finalproject/scripts/attribution.js
+++ finalproject/scripts/attribution.js
@@ -1,15 +1,3 @@
-const params = new URLSearchParams(window.location.search);
-
-const result = document.querySelector('#result');
-
-result.innerHTML = `
-<p><strong>Name:</strong> ${params.get("name")}</p>
-<p><strong>Email:</strong> ${params.get("email")}</p>
-<p><strong>Favorite Anime:</strong> ${params.get("anime")}</p>
-<p><strong>Favorite Genre:</strong> ${params.get("genre")}</p>
-<p><strong>Comments:</strong> ${params.get("comment")}</p>
-`
-
 const navbutton = document.querySelector('#ham-btn');
 const navbar = document.querySelector('#nav-bar');
 
